b4dcf043a9e0615d12a9a73260ade45d94b94b02
Irmak Çağlayan <127547865+irmakcaglayan@users.noreply.github.com>
2024-12-22 21:47:49 +0300

    guessTheNumber C++ TÜRKÇE
    Bu program, 1 ile 250 arası rastgele bir sayı seçer. 
    Ardından kullanıcıdan bu sayıyı tahmin etmesini bekler. 
    Kullanıcının tahmin ettiği sayı, programın seçtiği sayıdan küçükse program "Daha Büyük Bir Sayı Tahmin Edin!" çıktısını verir.  
    Kullanıcının tahmin ettiği sayı, programın seçtiği sayıdan büyükse program "Daha Küçük Bir Sayı Tahmin Edin!" çıktısını verir.
    Kullanıcı, programın seçtiği sayıyı doğru tahmin ettiğinde program "Tebrikler! x denemede doğru tahmin ettin." çıktısını verir.
     ( x yerine kullanıcının kaç tahminde bulunduğu gelir. Örneğin: "Tebrikler! 12 denemede doğru tahmin ettin.")

---
+++ guessTheNumberC++.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    srand(time(0));
+    int rastgeleSayi = rand() % 250 + 1;
+    int tahmin = 0;
+    int denemeler = 0;
+
+    cout << "** TAHMİN OYUNUNA HOŞ GELDİN! **\n";
+    cout << "1 ile 250 arasında bir sayı tuttum. Doğru tahmin etmeyi dene!\n";
+
+    while (tahmin != rastgeleSayi)
+    {
+        cout << "Tahmininizi Girin: "; cin >> tahmin;
+        denemeler++;
+
+        if (tahmin < rastgeleSayi)
+        {
+            cout << "Daha Büyük Bir Sayı Tahmin Edin!\n";
+        }
+        else if (tahmin > rastgeleSayi)
+        {
+            cout << "Daha Küçük Bir Sayı Tahmin Edin!\n";
+        }
+        else
+        {
+            cout << "TEBRİKLER! " << denemeler << " denemede doğru tahmin ettin.\n";
+        }
+    }
+    return 0;
+}
